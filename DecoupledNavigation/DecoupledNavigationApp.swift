a90633c5c3ce9b14c1cf25594d891f9c24139582
Hishara Dilshan <hisharadilshan3@gmail.com>
2024-11-12 04:33:23 +0000

    Initial Commit

---
+++ DecoupledNavigation/DecoupledNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecoupledNavigationApp.swift
+//  DecoupledNavigation
+//
+//  Created by Hishara Dilshan on 12/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct DecoupledNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DecoupledNavigationTests/DecoupledNavigationTests.swift
@@ -0,0 +1,17 @@
+//
+//  DecoupledNavigationTests.swift
+//  DecoupledNavigationTests
+//
+//  Created by Hishara Dilshan on 12/11/2024.
+//
+
+import Testing
+@testable import DecoupledNavigation
+
+struct DecoupledNavigationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
